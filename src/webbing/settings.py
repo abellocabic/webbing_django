212cc80ad8ea2d039af9eb5bb0bcb9b3f14f8316
abellocabic <abellocabic@gmail.com>
2016-05-13 17:32:06 +0200

    business casual is on

--- src/webbing/settings.py
+++ src/webbing/settings.py
@@ -119,4 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
+#This folder is indicating to go inside the virtualenv
--- src/webbing/urls.py
+++ src/webbing/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -21,3 +25,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^forum/', include('forum.urls', namespace="forum")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
